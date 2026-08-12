f826cbf5c82a176565524a69d20f6d7e388af7e6
Milan Popovic <popovic-milan@hotmail.com>
2026-08-12 13:28:18 +0200

    Configure H2 for integration tests

--- user-service/src/test/java/com/hotel/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/hotel/userservice/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.hotel.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
 	@Test
